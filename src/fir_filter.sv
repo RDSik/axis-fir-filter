f06c24ace9bf342f08081db4f6a6da0cfb0ba22c
RDSik <dmitryryabickov@yandex.ru>
2025-01-02 15:19:28 +0300

    Upd axis workflow

--- src/fir_filter.sv
+++ src/fir_filter.sv
@@ -16,8 +16,9 @@ module fir_filter #(
 
     initial $readmemh(COE_MEM, coe_mem);
 
-    logic signed [((COE_NUM-1)*DATA_WIDTH)-1:0] reg_o;
-    logic signed [((COE_NUM-1)*DATA_WIDTH)-1:0] acc_o;
+    logic signed [COE_NUM-2:0][DATA_WIDTH-1:0] reg_o;
+    logic signed [COE_NUM-2:0][DATA_WIDTH-1:0] acc_o;
+    logic signed [COE_NUM-1:0][COE_WIDTH-1:0]  coe_i;
 
     logic signed [DATA_WIDTH-1:0] fir_o;
     logic signed [DATA_WIDTH-1:0] fir_i;
@@ -28,48 +29,50 @@ module fir_filter #(
 
     genvar i;
     generate
-        for (i  = 0; i < COE_NUM; i = i + 1) begin : generate_block_identifier
+        for (i  = 0; i < COE_NUM; i = i + 1) begin : generate_block_identifier            
+            assign coe_i[i] = coe_mem[i];
+
             if (i == 0) begin
                 acc_reg #(
                     .DATA_WIDTH (DATA_WIDTH),
                     .COE_WIDTH  (COE_WIDTH )
-                ) i_areg (
-                    .clk_i  (clk_i                                 ), 
-                    .arstn_i(arstn_i                               ),
-                    .en_i   (reg_en                                ),
-                    .reg_i  (fir_i                                 ),
-                    .reg_o  (reg_o[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
-                    .coe_i  (coe_mem[i]                            ),
-                    .acc_i  ('0                                    ),
-                    .acc_o  (acc_o[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
+                ) i_acc_areg_f (
+                    .clk_i  (clk_i   ), 
+                    .arstn_i(arstn_i ),
+                    .en_i   (reg_en  ),
+                    .reg_i  (fir_i   ),
+                    .reg_o  (reg_o[i]),
+                    .coe_i  (coe_i[i]),
+                    .acc_i  ('0      ),
+                    .acc_o  (acc_o[i])
                 );
             end else if (i == COE_NUM - 1) begin
                 acc_reg #(
                     .DATA_WIDTH (DATA_WIDTH),
                     .COE_WIDTH  (COE_WIDTH )
-                ) i_areg (
-                    .clk_i  (clk_i                                 ), 
-                    .arstn_i(arstn_i                               ),
-                    .en_i   (reg_en                                ),
-                    .reg_i  (reg_o[i*DATA_WIDTH-1:(i-1)*DATA_WIDTH]),
-                    .reg_o  (                                      ),
-                    .coe_i  (coe_mem[i]                            ),
-                    .acc_i  (acc_o[i*DATA_WIDTH-1:(i-1)*DATA_WIDTH]),
-                    .acc_o  (fir_o                                 )
+                ) i_acc_reg_l (
+                    .clk_i  (clk_i     ),
+                    .arstn_i(arstn_i   ),
+                    .en_i   (reg_en    ),
+                    .reg_i  (reg_o[i-1]),
+                    .reg_o  (          ),
+                    .coe_i  (coe_i[i]  ),
+                    .acc_i  (acc_o[i-1]),
+                    .acc_o  (fir_o     )
                 );
             end else begin
                 acc_reg #(
                     .DATA_WIDTH (DATA_WIDTH),
                     .COE_WIDTH  (COE_WIDTH )
-                ) i_areg (
-                    .clk_i  (clk_i                                 ), 
-                    .arstn_i(arstn_i                               ),
-                    .en_i   (reg_en                                ),
-                    .reg_i  (reg_o[i*DATA_WIDTH-1:(i-1)*DATA_WIDTH]),
-                    .reg_o  (reg_o[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
-                    .coe_i  (coe_mem[i]                            ),
-                    .acc_i  (acc_o[i*DATA_WIDTH-1:(i-1)*DATA_WIDTH]),
-                    .acc_o  (acc_o[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH])
+                ) i_areg_n (
+                    .clk_i  (clk_i     ), 
+                    .arstn_i(arstn_i   ),
+                    .en_i   (reg_en    ),
+                    .reg_i  (reg_o[i-1]),
+                    .reg_o  (reg_o[i]  ),
+                    .coe_i  (coe_i[i]  ),
+                    .acc_i  (acc_o[i-1]),
+                    .acc_o  (acc_o[i]  )
                 );
             end
         end
@@ -77,30 +80,39 @@ module fir_filter #(
 
     always @(posedge clk_i or negedge arstn_i) begin
         if (~arstn_i) begin
-            fir_cnt      <= '0;
             m_axis.tdata <= '0;
             fir_i        <= '0;
-        end else if (fir_cnt == COE_NUM - 1) begin
-            fir_cnt      <= '0;
-            m_axis.tdata <= fir_o;
-            fir_i        <= s_axis.tdata;
-        end else if (~s_axis.tvalid | ~m_axis.tready) begin
-            fir_cnt <= COE_NUM - 1;
-        end else begin
-            fir_cnt <= fir_cnt + 1;
-            fir_i   <= s_axis.tdata;
+        end else begin 
+            if (s_axis.tready & s_axis.tvalid) begin
+                fir_i <= s_axis.tdata;
+            end 
+            if ((m_axis.tready & m_axis.tvalid) || (fir_cnt == COE_NUM - 1)) begin
+                m_axis.tdata <= fir_o;
+            end
         end
     end
 
     always_ff @(posedge clk_i or negedge arstn_i) begin
-        if (~arstn_i | ~s_axis.tvalid | ~m_axis.tready) begin
+        if (~arstn_i) begin
             s_axis.tready <= 1'b0;
             m_axis.tvalid <= 1'b0;
             reg_en        <= 1'b0;
+            fir_cnt       <= '0;
         end else begin
-            s_axis.tready <= 1'b1;
-            m_axis.tvalid <= 1'b1;
-            reg_en        <= 1'b1;
+            if ((s_axis.tready & s_axis.tvalid) || (m_axis.tready & m_axis.tvalid)) begin
+                reg_en  <= 1'b1;
+                if (fir_cnt == COE_NUM - 1) begin
+                    fir_cnt <= '0;    
+                end else begin 
+                    fir_cnt <= fir_cnt + 1'b1;
+                end
+            end
+            if (fir_cnt == COE_NUM - 1) begin
+                m_axis.tvalid <= 1'b1;
+            end 
+            if (s_axis.tvalid) begin
+                s_axis.tready <= 1'b1;
+            end
         end
     end
 
